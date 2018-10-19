8e7be428494657fa12696985a2fc747e7526bc6b
ArthurR <rebre.arthur@gmail.com>
2018-10-19 10:40:12 +0200

    Ajout du front

---
+++ front/js/app.js
@@ -0,0 +1,34 @@
+$(function() {
+    
+    /*MENU*/
+    $("#btn_menu").click(function(){
+        
+        if($(".main_menu").hasClass("menu_active")){
+            $(".main_menu").removeClass("menu_active");
+            $(".fa-bars").removeClass("hide");
+            $(".fa-times").addClass("hide");
+        }
+        else{
+            $(".main_menu").addClass("menu_active");
+            $(".fa-bars").addClass("hide");
+            $(".fa-times").removeClass("hide");
+        }
+    });
+
+    $("main").click(function(){
+
+        if($(".main_menu").hasClass("menu_active")){
+            $(".main_menu").removeClass("menu_active");
+            $(".fa-bars").removeClass("hide");
+            $(".fa-times").addClass("hide");
+        }
+        else{
+        }
+    });
+
+    if($('#item_modal').hasClass("show")){
+        $("canvas").css("opacity", "1");
+    }
+    else{}
+
+});
